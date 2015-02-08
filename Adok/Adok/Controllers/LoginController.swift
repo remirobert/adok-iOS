91caecc646f47ee582bcd65ba76672e4d771c17d
rémi <remirobert33530@gmail.com>
2015-02-08 12:13:23 +0100

    controllers

---
+++ Adok/Adok/Controllers/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  Adok
+//
+//  Created by Remi Robert on 08/02/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
