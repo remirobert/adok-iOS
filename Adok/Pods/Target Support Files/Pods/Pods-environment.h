630e048ef4c635b36bc4c25d2d959292aeeeba97
rémi <remirobert33530@gmail.com>
2015-02-09 11:50:33 +0100

    SDWebImage

--- Adok/Pods/Target Support Files/Pods/Pods-environment.h
+++ Adok/Pods/Target Support Files/Pods/Pods-environment.h
@@ -84,6 +84,18 @@
 #define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 4
 #define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 4
 
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 1
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 1
+
 // SimpleAuth/Core
 #define COCOAPODS_POD_AVAILABLE_SimpleAuth_Core
 #define COCOAPODS_VERSION_MAJOR_SimpleAuth_Core 0
